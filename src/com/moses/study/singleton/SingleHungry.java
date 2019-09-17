5ba83fbffadba2499d661c2ed41b3438c0560bc2
Moses Ji <jimuchen@163.com>
2019-09-17 15:41:42 +0800

    recreate

--- src/com/moses/study/singleton/SingleHungry.java
+++
@@ -1,7 +0,0 @@
-package com.moses.study.singleton;
-
-public class SingleHungry {
-	public static SingleHungry instance = new SingleHungry();
-	private SingleHungry() {}
-	
-}
--- src/com/moses/study/threadFramework/entity/TaskResultType.java
+++
@@ -1,7 +0,0 @@
-package com.moses.study.threadFramework.entity;
-
-public enum TaskResultType {
-	Success,
-	Failure,
-	Exception;
-}
